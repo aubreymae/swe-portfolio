d558a224f0f7c34ddadba108eb572c099f3077ce
Aubrey Garcia <aubreymaerg@gmail.com>
2026-05-02 14:02:07 -0400

    Updated Works page to include new WorksList component

--- src/pages/Works/WorksView.tsx
+++ src/pages/Works/WorksView.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+import { Work, works } from "../../data/selected-works";
+
+import WorksList from "../../components/WorksList/WorksList";
 import "./WorksView.css";
 
 export default function WorksView() {
+  const [selectedWork, setSelectedWork] = useState<Work | null>(works[0]);
+
+  const onSelectWork = (id: string) => {
+    const work = works.find((w) => w.id === id);
+    setSelectedWork(work || null);
+  };
+
   return (
-    <>
+    <div id="works-view-container">
+      <WorksList onClick={onSelectWork} selectedWorkId={selectedWork?.id} />
       <section className="main-grid">
         <div className="grid-col">
           <div className="info-wrapper">
             <h3>Release Date</h3>
             <div className="detail-container">
-              <p>April 2026</p>
+              <p>{selectedWork?.release}</p>
             </div>
           </div>
         </div>
@@ -16,11 +28,11 @@ export default function WorksView() {
           <div className="info-wrapper">
             <h3>Project Name</h3>
             <div className="detail-container">
-              <p>CycleSpots</p>
+              <p>{selectedWork?.title}</p>
             </div>
           </div>
         </div>
       </section>
-    </>
+    </div>
   );
 }
